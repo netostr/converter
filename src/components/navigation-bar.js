1b6438eefd170f87751e55a187b7104ad54c68ea
Bakiev Damir <8neto8@bk.ru>
2021-05-21 21:38:57 +0500

    add avatar

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import SwipeableTemporaryDrawer from './drawer';
+import Avatar from '@material-ui/core/Avatar';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,31 +24,32 @@ const useStyles = makeStyles((theme) => ({
 export default function ButtonAppBar() {
   const classes = useStyles();
 
-  const [isDriverOpen, setDriverOpen] = React.useState(false); 
+  const [isDriverOpen, setDriverOpen] = React.useState(false);
+  const [isAvatarShow, setAvatarShow] = React.useState(false);
 
   const toggleDrawer = (open) => (event) => {
     if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
     }
-
     setDriverOpen(open);
   };
 
   return (
-    <>
-        <SwipeableTemporaryDrawer state={isDriverOpen} toggleDrawer={toggleDrawer}/>
+    <>        
         <div className={classes.root}>
-        <AppBar position="static">
-            <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer(!isDriverOpen)}>
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-                News
-            </Typography>
-            <Button color="inherit">Login</Button>
-            </Toolbar>
-        </AppBar>
+          <SwipeableTemporaryDrawer state={isDriverOpen} toggleDrawer={toggleDrawer}/>
+          <AppBar position="static">
+              <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer(!isDriverOpen)}>
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    News
+                </Typography>
+                {isAvatarShow ? <Avatar alt="cat" src="/catAvatar.jpg"/> : null}
+                <Button color="inherit" onClick={() => setAvatarShow(!isAvatarShow)}>{isAvatarShow ? 'Logout' : 'Login'}</Button>
+              </Toolbar>
+          </AppBar>
         </div>
     </>
   );
